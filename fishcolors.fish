0155ed3f66c424d3f617465706c5ab697ccba621
Cayden de Wit <cayden@caydenarchdesktop.localdomain>
2020-10-26 16:31:40 +0200

    Added fish colorscheme

---
+++ fishcolors.fish
@@ -0,0 +1,11 @@
+set -U fish_color_operator 802444
+set -U fish_color_escape 802444
+set -U fish_color_autosuggestion 777777
+set -U fish_color_comment 555555
+set -U fish_color_param default
+set -U fish_color_error f53054
+set -U fish_color_end 3f37b3
+set -U fish_color_redirection 312684
+set -U fish_color_quote 4c2ca4
+set -U fish_color_command default
+
